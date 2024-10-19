ecd67931506681e81f36c953094854a63f2a1762
kasimD1 <164982755+kasimD1@users.noreply.github.com>
2024-10-19 21:24:21 +0000

    changes

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,64 @@
-console.log(" hello world ");
+// console.log(" hello world ");
+// var accountTitle="Qasim Nawaz ";
+// let accountCity="Lahore";
+// let accountstate;
+// const accountId="56263563";
+// accountType="Saving account ";
+ accountCity="Islamabad";
+// accountTitle="Hamza Noor"
+// console.log(accountId);
+// console.table([accountId,accountCity,accountTitle,accountType,accountstate]); 
+
+
+
+// let nam="33";
+// let convetedNum=Number(nam);
+// console.log(nam);
+// console.log(typeof nam);
+// console.log(convetedNum);
+// console.log(typeof convetedNum);
+
+// let nam="33abc";
+// let convetedNum=Number(nam);
+// console.log(nam);
+// console.log(typeof nam);
+// console.log(convetedNum);
+// console.log(typeof convetedNum);
+
+// the string is successully converted int number if it can otherwise it show the "number " after conversion 
+//on calling typeof func but when we try to print the number it show NaN.
+
+
+//  let bol=false;
+//  let convetedNum=Number(bol);
+//  console.log(bol);
+//  console.log(typeof bol);
+//  console.log(convetedNum);
+//  console.log(typeof convetedNum);
+
+//  let bol=true;
+//  let convetedNum=Number(bol);
+//  console.log(bol);
+//  console.log(typeof bol);
+//  console.log(convetedNum);
+//  console.log(typeof convetedNum);
+
+//if the boolean value is converted into number. it should be successfully converted into the number acc to type and val both
+//false<=0
+//true<=1
+
+
+// console.log(null);
+// console.log(typeof null);
+// console.log(undefined);
+// console.log(typeof undefined);
+// let numberfromNull=Number(null);
+// console.log(numberfromNull);
+// console.log(typeof numberfromNull);
+// let numberfromNull=Number(undefined);
+// console.log(numberfromNull);
+// console.log(typeof numberfromNull);
+
+
+//undefined<= NaN
+//null<= 0
